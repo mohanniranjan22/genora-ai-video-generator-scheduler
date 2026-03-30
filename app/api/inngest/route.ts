f73a467cbd9e32a8313684d628beddd79826588d
mohanniranjan22 <mohanniranjan9999@gmail.com>
2026-03-30 20:56:11 +0530

    sample

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -10,8 +10,6 @@ import {
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  signingKey: process.env.INNGEST_SIGNING_KEY,
-  landingPage: true,
   functions: [
     helloWorld,
     generateVideo,
